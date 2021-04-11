061fe3cc703f0a8eaa713181adc7eab72acedf19
yousef-ghanim <yousefghanim85@gmail.com>
2021-04-11 21:06:10 +0000

    Updates

---
+++ precache-manifest.a0888b6dfa9e0cd0bcd043d489ea7d0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e9b5c1256fc054e3e64c6ac6abb3f1",
+    "url": "/grocery-bud-react/index.html"
+  },
+  {
+    "revision": "c534fd05bbe2f6cc85ba",
+    "url": "/grocery-bud-react/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/grocery-bud-react/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-bud-react/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c534fd05bbe2f6cc85ba",
+    "url": "/grocery-bud-react/static/js/main.c17d6f42.chunk.js"
+  },
+  {
+    "revision": "6c50ee383b8230110b16",
+    "url": "/grocery-bud-react/static/js/runtime-main.373aa889.js"
+  }
+]);
